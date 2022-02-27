9202bc1fc9be26914fc474b63ef2cd2cf95a671d
ante <ante.kotarac@gmail.com>
2022-02-27 21:38:58 +0100

    player stub

---
+++ player.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+    def update(self): pass
+    def move(self): pass
+    def check_collission(self): pass
