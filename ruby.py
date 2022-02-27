23505ef37f3e7c77e080f56deb7401d5fb058b9e
ante <ante.kotarac@gmail.com>
2022-02-27 21:51:59 +0100

    more sprite stubs

---
+++ ruby.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Ruby(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+    def update(self): pass
+    def check_collission(self): pass
+
+    def animate(self): pass
+
